e8ffa589c34767dda3eddf9b268f5aab46dd79b4
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-26 01:46:48 +0100

    first commit

---
+++ src/main/java/com/eds/encrypted_data_sharing_svc/EncryptedDataSharingSvcApplication.java
@@ -0,0 +1,13 @@
+package com.eds.encrypted_data_sharing_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptedDataSharingSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptedDataSharingSvcApplication.class, args);
+	}
+
+}
