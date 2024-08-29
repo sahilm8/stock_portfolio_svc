c4e1caac987309d7950ed94052d1c52030070cfb
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-29 03:51:07 +0100

    Update app properties, add key pair gen util

--- src/main/java/com/eds/encrypted_data_sharing_svc/EncryptedDataSharingSvcApplication.java
+++ src/main/java/com/eds/encrypted_data_sharing_svc/EncryptedDataSharingSvcApplication.java
@@ -1,13 +1,35 @@
 package com.eds.encrypted_data_sharing_svc;
 
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.eds.encrypted_data_sharing_svc.util.KeyPairGeneratorUtil;
 
 @SpringBootApplication
 public class EncryptedDataSharingSvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptedDataSharingSvcApplication.class, args);
+		try {
+			KeyPair keyPair = KeyPairGeneratorUtil.generateKeyPair();
+			PublicKey publicKey = keyPair.getPublic();
+			PrivateKey privateKey = keyPair.getPrivate();
+
+			// Encode keys to Base64 for storage
+			String publicKeyBase64 = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+			String privateKeyBase64 = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+
+			System.out.println("Public Key: " + publicKeyBase64);
+			System.out.println("Private Key: " + privateKeyBase64);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/main/java/com/eds/encrypted_data_sharing_svc/util/KeyPairGeneratorUtil.java
@@ -0,0 +1,14 @@
+package com.eds.encrypted_data_sharing_svc.util;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class KeyPairGeneratorUtil {
+
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048); // Key size
+        return keyPairGenerator.generateKeyPair();
+    }
+}
