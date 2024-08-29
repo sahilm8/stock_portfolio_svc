cb357bbccc3ed7b2bb78cda0d5f4bb65d6366f5d
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-29 11:16:58 +0100

    fix build, update service

--- src/test/java/com/eds/encrypted_data_sharing_svc/EncryptedDataSharingSvcApplicationTests.java
+++ src/test/java/com/eds/encrypted_data_sharing_svc/EncryptedDataSharingSvcApplicationTests.java
@@ -1,13 +1,7 @@
 package com.eds.encrypted_data_sharing_svc;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EncryptedDataSharingSvcApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
