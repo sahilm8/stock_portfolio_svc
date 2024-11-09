b5f5a729a84dfedddbe803d2b295b28aa3ddcd29
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-11-09 07:49:52 +0000

    Remove old files

--- src/main/java/com/eds/encrypted_data_sharing_svc/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eds.encrypted_data_sharing_svc.repository;
-
-import com.eds.encrypted_data_sharing_svc.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
