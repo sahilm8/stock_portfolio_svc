8bfaec4752e995d8481546a94229c9ebf8b29073
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-11-21 06:28:49 +0000

    Anotate repositories with @Repository

--- src/main/java/com/finance/repository/UserRepository.java
+++ src/main/java/com/finance/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.finance.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finance.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByEmail(String email);
 }
