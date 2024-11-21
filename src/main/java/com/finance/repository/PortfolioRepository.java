d0b228341970bff43a2a22b9481c0e6be5a346c3
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-11-21 01:11:35 +0000

    Update portfolio repository and create createPortfolio svc method

--- src/main/java/com/finance/repository/PortfolioRepository.java
+++ src/main/java/com/finance/repository/PortfolioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.finance.model.Portfolio;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-    public Portfolio getByPortfolioName(String portfolioName);
+    public Portfolio findByPortfolioName(String portfolioName);
 }
---
+++ src/main/java/com/finance/service/PortfolioService.java
@@ -0,0 +1,27 @@
+package com.finance.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finance.model.Portfolio;
+import com.finance.repository.PortfolioRepository;
+
+@Service
+public class PortfolioService {
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+
+    public String createPortfolio(Portfolio portfolio) {
+        if (portfolioRepository.findByPortfolioName(portfolio.getPortfolioName()) == null) {
+            portfolio.setPortfolioValue(
+                    portfolio.getStocks().stream()
+                            .map(stock -> stock.getStockPrice().multiply(stock.getStockQuantity()))
+                            .reduce(BigDecimal.ZERO, BigDecimal::add));
+            portfolioRepository.save(portfolio);
+            return "Portfolio has been created successfully.";
+        }
+        return "Portfolio with the given name already exists.";
+    }
+}
