359584c3e9ab976e48be8991f70eb0a0cc850841
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-24 01:08:25 +0000

    Remove redundant method signature from Order JPA repo

--- src/main/java/com/sahil/stock/portfolio/repository/OrderRepository.java
+++ src/main/java/com/sahil/stock/portfolio/repository/OrderRepository.java
@@ -1,7 +1,5 @@
 package com.sahil.stock.portfolio.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,4 @@ import com.sahil.stock.portfolio.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    public Optional<Order> findByName(String name);
 }
