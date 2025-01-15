74c491339a7b5de1a7b81d9bd0ef69a9c50a460b
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-15 13:10:39 +0000

    Create DTOs for addPortfolio

---
+++ src/main/java/com/sahil/stock/portfolio/dto/AddPortfolioRequest.java
@@ -0,0 +1,22 @@
+package com.sahil.stock.portfolio.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddPortfolioRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotBlank(message = "Currency is required")
+    private String currency;
+}
---
+++ src/main/java/com/sahil/stock/portfolio/dto/AddPortfolioResponse.java
@@ -0,0 +1,16 @@
+package com.sahil.stock.portfolio.dto;
+
+import com.sahil.stock.portfolio.model.Portfolio;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddPortfolioResponse {
+    private Portfolio portfolio;
+}
