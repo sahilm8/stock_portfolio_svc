57b95c14cd77ff26fdf4dc084f4665e35020f563
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-16 02:33:33 +0000

    Update Application, remove redundant files

--- src/main/java/com/sahil/stock/portfolio/Application.java
+++ src/main/java/com/sahil/stock/portfolio/Application.java
@@ -3,16 +3,9 @@ package com.sahil.stock.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootApplication
-@Slf4j
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		log.info("*** APPLICATION SERVICE ACTIVATED ***");
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.info("*** APPLICATION SERVICE TERMINATED ***");
-        }));
 	}
 }
