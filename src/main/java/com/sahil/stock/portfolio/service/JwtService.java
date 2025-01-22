cca553a87a2f69d2eaf4b6378907e869001b40b9
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-22 02:09:38 +0000

    Setup JwtService

---
+++ src/main/java/com/sahil/stock/portfolio/service/JwtService.java
@@ -0,0 +1,40 @@
+package com.sahil.stock.portfolio.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractClaim(token, Claims::getSubject);
+        final Date expiration = extractClaim(token, Claims::getExpiration);
+        return (username.equals(userDetails.getUsername())) && new Date().before(expiration);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsResolver.apply(claims);
+    }
+}
