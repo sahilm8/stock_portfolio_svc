fe3a9edc47d338bfe1ec767f6797411e0fa10227
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-22 02:23:47 +0000

    Setup relationships between User, Portfolio, & Transaction

--- src/main/java/com/sahil/stock/portfolio/model/Portfolio.java
+++ src/main/java/com/sahil/stock/portfolio/model/Portfolio.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -46,6 +48,10 @@ public class Portfolio {
 
     @Column(nullable = false, precision = 10, scale = 4)
     private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
     private List<Stock> stocks;
--- src/main/java/com/sahil/stock/portfolio/model/User.java
+++ src/main/java/com/sahil/stock/portfolio/model/User.java
@@ -1,10 +1,10 @@
 package com.sahil.stock.portfolio.model;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -12,11 +12,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,30 +24,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "transaction")
-public class Transaction {
+@Table(name = "users")
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private Date createdAt;
 
+    @UpdateTimestamp
     @Column(nullable = false)
-    private String currency;
+    private Date updatedAt;
 
-    @Column(nullable = false, precision = 10, scale = 4)
-    private BigDecimal amount;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Portfolio> portfolios;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "portfolio_id", nullable = false)
-    private Portfolio portfolio;
-
-    @OneToMany(mappedBy = "transaction", cascade = CascadeType.ALL)
-    private List<Stock> stocks;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
 }
