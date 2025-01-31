4cb39a7de91780ebd42fa06a60c86ae13e61638e
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-31 09:02:25 +0000

    Update response DTOs

--- src/main/java/com/sahil/stock/portfolio/dto/getPortfolio/GetPortfolioResponse.java
+++ src/main/java/com/sahil/stock/portfolio/dto/getPortfolio/GetPortfolioResponse.java
@@ -1,6 +1,12 @@
 package com.sahil.stock.portfolio.dto.getPortfolio;
 
-import com.sahil.stock.portfolio.model.Portfolio;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import com.sahil.stock.portfolio.model.Stock;
+import com.sahil.stock.portfolio.model.Transaction;
+import com.sahil.stock.portfolio.model.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,5 +18,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetPortfolioResponse {
-    private Portfolio portfolio;
+    private Long id;
+    private Date createdAt;
+    private String name;
+    private String description;
+    private String currency;
+    private BigDecimal amount;
+    private User user;
+    private List<Stock> stocks;
+    private List<Transaction> transactions;
 }
