c375ee3f86746f45c91bdf06447ce48f0f004e43
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-02-04 02:53:50 +0000

    Create DTOs for refreshToken

---
+++ src/main/java/com/sahil/stock/portfolio/dto/refreshToken/RefreshTokenResponse.java
@@ -0,0 +1,14 @@
+package com.sahil.stock.portfolio.dto.refreshToken;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    private String accessToken;
+}
